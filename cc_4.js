134d8b1e9060fa0df283554f1cbde626d6db544d
Jordon Venner <jordonvenner@usf.edu>
2025-02-02 14:42:17 -0500

    Task 2 - Sales Report.

--- cc_4.js
+++ cc_4.js
@@ -8,3 +8,16 @@ if (purchaseAmount > 100) {
 }
 
 console.log(`Final amount after discount: $${purchaseAmount}`);
+
+
+//Task 2: For Loop
+
+let sales = [155, 65, 321, 110, 10];
+
+let totalSales = 0;
+
+for (let i = 0 < sales.length; i++) {
+    totalSales += sales [i];
+}
+
+console.log(`Total sales: $${tatalSales}`);
